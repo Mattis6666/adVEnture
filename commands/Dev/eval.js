b1ac8c6e7386df4503d8b2a7831a000f4e1677ab
Mattis6666 <sgtmattis@gmail.com>
2020-03-07 22:24:20 +0100

    fixed eval

--- commands/Dev/eval.js
+++ commands/Dev/eval.js
@@ -13,7 +13,6 @@ module.exports = {
         const db = require('../../utility/mongo.js');
         const functions = require('../../utility/functions.js');
         const nekos = require('../../utility/nekos.js');
-        const ttt = require('../../utility/tictactoe.js');
         const config = require('config');
         let output;
         try {
