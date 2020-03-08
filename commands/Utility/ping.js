04e5f441f05bfe2ed58853bd84f4681ddc440ecc
Mattis6666 <sgtmattis@gmail.com>
2020-03-08 16:45:57 +0100

    Now sending, then editing for more accuracy

--- commands/Utility/ping.js
+++ commands/Utility/ping.js
@@ -9,6 +9,8 @@ module.exports = {
     args: false,
     category: 'Utility',
     execute(message) {
-        message.channel.send(`Pong! \`${new Date().getTime() - message.createdTimestamp} ms\``);
+        message.channel.send('Pinging...').then(sent => {
+            sent.edit(`Pong! Took \`${sent.createdTimestamp - message.createdTimestamp}ms\`.`);
+        });
     }
 };
